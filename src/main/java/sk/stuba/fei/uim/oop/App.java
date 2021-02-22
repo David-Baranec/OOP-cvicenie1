eda9c0476712d831bd1697fced111e89f9677e00
David_Baranec <Hyb.3.fur.zix>
2021-02-22 13:44:50 +0100

    som si pridal KeyboardInput

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,10 +2,28 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello world");
-        System.out.println("Hello world, one more time");
-        System.out.println("I am bored");
-        System.out.println("what has happened?");
+
+        uloha1();
+        System.out.println("Vrátilo mi:" + uloha2());
+
     }
 
-}
+    static void uloha1() {
+        System.out.println("Hello World");
+    }
+
+    static int uloha2() {
+        int prveCislo=KeyboardInput.readInt("Zadaj prve cislo: ");
+        int druheCislo=KeyboardInput.readInt("Zadaj druhe cislo: ");
+        if (prveCislo > druheCislo) {
+            return prveCislo;
+        } else if (prveCislo == druheCislo) {
+            return 0;
+        } else {
+            return druheCislo;
+        }
+    }
+
+    }
+
+
